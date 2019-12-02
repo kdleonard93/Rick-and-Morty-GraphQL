3f28c291e2046f52e87275927123dfdd17db12b0
Kyle Leonard <kdleonard@icloud.com>
2019-12-02 00:05:44 -0600

    GQL progress

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,28 +1,48 @@
 import React, { useEffect, useState } from "react";
 import CharCard from "./CharacterCard";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
   const [characters, setCharacters] = useState([]);
 
+  const CHAR_QUERY = gql`
+    {
+      characters(page: 2, filter: { name: "rick" }) {
+        info {
+          count
+        }
+        results {
+          name
+        }
+      }
+      character(id: 1) {
+        id
+      }
+    }
+  `;
+
   return (
-    <section className="character-list grid-view">
-      {characters.map(char => {
-        return (
-          <div>
-            <CharacterCard
-              key={char.name}
-              name={char.name}
-              species={char.species}
-              gender={char.gender}
-              origin={char.origin}
-              location={char.location}
-              status={char.status}
-            />
-          </div>
-        );
-      })}
-    </section>
+    <Query query={CHAR_QUERY}>
+      <section className="character-list grid-view">
+        {characters.map(char => {
+          return (
+            <div>
+              <CharCard
+                key={char.name}
+                name={char.name}
+                species={char.species}
+                gender={char.gender}
+                origin={char.origin}
+                location={char.location}
+                status={char.status}
+              />
+            </div>
+          );
+        })}
+      </section>
+    </Query>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const httpLink = createHttpLink({
+  uri: "https://rickandmortyapi.com/graphql/"
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
